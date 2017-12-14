a447ba090bdcf61ebfc77259a7ecc38fac663ff1
Karl Jahn <kjahn@amelco.co.uk>
2017-12-14 21:56:29 +0000

    Add logging information into script

--- scripts/tableDataPopulator.sh
+++ scripts/tableDataPopulator.sh
@@ -6,6 +6,7 @@
 #Collect script info
 script_name=$( basename $0 )
 dir_name=$( dirname $0  )
+log_file=$(echo $script_name | cut -d'.' -f 1).log
 
 repo_home=~/git/speed-test-rpi
 
@@ -13,8 +14,12 @@ cd "${dir_name}"/../
 
 function log() {
     currDate=$( date "+%Y%m%d-%H%M%S" )
+    currDay=$( date "+%Y%m%d" )
     msg=$1
     echo "${currDate}   -   ${msg}"
+
+    mkdir -p logs
+    echo "${currDate}   -   ${msg}" > ${log_file}-${currDay}.log
 }
 
 log "TableData updater started"
@@ -32,5 +37,7 @@ log "Update git repo"
 git add .
 
 git commit -m "Automated - Update speedtest table data"
+
+git push
 
 log "TableData updater ended"

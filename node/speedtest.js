042f138394522b54c5e1a45230f0b4faf0e00c11
Karl Jahn <kjahn@amelco.co.uk>
2017-12-14 21:15:41 +0000

    Add tableData populator. Still pending to handle edge scenarios, i.e. max-file-limit

--- node/speedtest.js
+++ node/speedtest.js
@@ -1,4 +1,12 @@
+var fs = require('fs');
+
 var currDate = Date.now();
+
+var tableData = JSON.parse(fs.readFileSync('tableData.js', 'utf8'));
+
+if (tableData == undefined){
+  tableData = [];
+}
 
 require('speedtest-net')().on('data', data => {
   
@@ -12,10 +20,24 @@ require('speedtest-net')().on('data', data => {
 
   // console.log("speed_download, speed_upload, speed_originalDownload, speed_originalUpload, clnt_ip, clnt_lat, clnt_lon, clnt_isp, clnt_isprating, clnt_rating, clnt_ispdlavg, clnt_ispuplavg, srv_host, srv_lat, srv_lon, srv_location, srv_country, srv_cc, srv_sponsor, srv_distanceKm, srv_distanceMi, srv_ping, srv_id");
 
-  // console.log(data['speeds']['download'] + ", " + data['speeds']['upload'] + ", " + data['speeds']['originalDownload'] + ", " + data['speeds']['originalUpload'] + ", " + data['client']['ip'] + ", " + data['client']['lat'] + ", " + data['client']['lon'] + ", " + data['client']['isp'] + ", " + data['client']['isprating'] + ", " + data['client']['rating'] + ", " + data['client']['ispdlavg'] + ", " + data['client']['ispulavg'] + ", " + data['server']['lat'] + ", " + data['server']['lon'] + ", " + data['server']['location'] + ", " + data['server']['country'] + ", " + data['server']['cc'] + ", " + data['server']['sponsor'] + ", " + data['server']['distance'] + ", " + data['server']['distanceMi'] + ", " + data['server']['ping'] + ", " + data['server']['id']);
-
+  console.log("New data gathered: ");
   console.dir(newData);
 
+  console.log("All entries: ");
   
+  tableData.push(newData);
+  console.dir(tableData);
+
+  var tableDataStr = JSON.stringify(tableData);
+
+  fs.writeFile("./tableData.js", tableDataStr, function(err) {
+  
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+
+  });
 
 });
